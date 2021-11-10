288a3e6eb588abc4ff519dc42f15521b67d9d567
kxxt <rsworktech@outlook.com>
2021-11-10 23:12:06 +0800

    Switch to KaTeX from MathJax.

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -1,16 +1,38 @@
-window.MathJax = {
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex|hidden-latex|md-nav__link"
-  }
-};
+(function () {
+  'use strict';
 
-document$.subscribe(() => {
-  MathJax.typesetPromise()
-})
+  var katexMath = (function () {
+    var maths = document.querySelectorAll('.arithmatex, .hidden-latex, .md-nav__link'),
+      tex;
+
+    for (var i = 0; i < maths.length; i++) {
+      tex = maths[i].textContent || maths[i].innerText;
+      if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false });
+      } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true });
+      }
+    }
+  });
+
+  (function () {
+    var onReady = function onReady(fn) {
+      if (document.addEventListener) {
+        document.addEventListener("DOMContentLoaded", fn);
+      } else {
+        document.attachEvent("onreadystatechange", function () {
+          if (document.readyState === "interactive") {
+            fn();
+          }
+        });
+      }
+    };
+
+    onReady(function () {
+      if (typeof katex !== "undefined") {
+        katexMath();
+      }
+    });
+  })();
+
+}());
