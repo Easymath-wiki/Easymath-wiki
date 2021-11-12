f52f1782d301cd9d7a461732683b8ceba29faa5c
kxxt <rsworktech@outlook.com>
2021-11-12 12:17:57 +0800

    Setup KaTeX branch.

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -1,16 +1,19 @@
+var macros = {};
 (function () {
   'use strict';
-
   var katexMath = (function () {
-    var maths = document.querySelectorAll('.arithmatex, .hidden-latex, .md-nav__link'),
-      tex;
-
+    var maths = document.querySelectorAll('.arithmatex, .hidden-latex, .md-nav__link'),tex;
+    // katex.renderToString("\\newcommand\\xfrac{\\displaystyle \\frac}",{macros, globalGroup: true});
     for (var i = 0; i < maths.length; i++) {
       tex = maths[i].textContent || maths[i].innerText;
+      if (maths[i].classList.contains('hidden-latex')) {
+        katex.renderToString(tex, { 'displayMode': true, 'macros': macros, 'globalGroup': true });
+        continue;
+      }
       if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
-        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false });
+        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false, 'macros': macros });
       } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
-        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true });
+        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true, 'macros': macros });
       }
     }
   });
