f207a846df5c675013d3ecd92c38a8cec8571d49
kxxt <rsworktech@outlook.com>
2021-11-12 13:21:32 +0800

    换回 MathJax，版本降级到 2.7.9。
    因为 KaTeX 导致了部分语法不兼容和公式编号与公式内容重叠。
    使用 MathJaX 与 Typora 更一致一些，虽然渲染速度比较慢。

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -1,38 +1,13 @@
-(function () {
-  'use strict';
-
-  var katexMath = (function () {
-    var maths = document.querySelectorAll('.arithmatex, .hidden-latex, .md-nav__link'),
-      tex;
-
-    for (var i = 0; i < maths.length; i++) {
-      tex = maths[i].textContent || maths[i].innerText;
-      if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
-        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false });
-      } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
-        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true });
-      }
-    }
-  });
-
-  (function () {
-    var onReady = function onReady(fn) {
-      if (document.addEventListener) {
-        document.addEventListener("DOMContentLoaded", fn);
-      } else {
-        document.attachEvent("onreadystatechange", function () {
-          if (document.readyState === "interactive") {
-            fn();
-          }
-        });
-      }
-    };
-
-    onReady(function () {
-      if (typeof katex !== "undefined") {
-        katexMath();
-      }
-    });
-  })();
-
-}());
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  extensions: ["tex2jax.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  tex2jax: {
+    inlineMath: [ ["\\(","\\)"] ],
+    displayMath: [ ["\\[","\\]"] ],
+    processEscapes: true,
+    processEnvironments: true,
+    ignoreClass: ".*|",
+    processClass: "arithmatex|hidden-latex"
+  },
+});
