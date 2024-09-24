3be6e73e906a0b0f0a47ed83d1c8f2c25d4ad68f
yuyuan12138 <yuyuan12138@users.noreply.github.com>
2024-09-24 01:41:28 +0000

    Deployed 12b046f with MkDocs version: 1.5.3

---
+++ javascripts/config.js
@@ -0,0 +1,13 @@
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  extensions: ["tex2jax.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  tex2jax: {
+    inlineMath: [ ["\\(","\\)"] ],
+    displayMath: [ ["\\[","\\]"] ],
+    processEscapes: true,
+    processEnvironments: true,
+    ignoreClass: ".*|",
+    processClass: "arithmatex|hidden-latex"
+  },
+});
